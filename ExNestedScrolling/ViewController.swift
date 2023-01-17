056dfcc6954bfc11c115600d59d62d85a46e7fbe
jake <palatable7@naver.com>
2023-01-17 22:43:31 +0900

    Fix

--- ExNestedScrolling/ViewController.swift
+++ ExNestedScrolling/ViewController.swift
@@ -80,7 +80,7 @@ extension ViewController: UITableViewDataSource {
 
 extension ViewController: UITableViewDelegate {
     private enum Metric {
-        static let reduceAcceacceleration = 5.0
+        static let reduceAcceleration = 5.0
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
@@ -120,7 +120,7 @@ extension ViewController: UITableViewDelegate {
         // inner scroll을 모두 less scroll한 경우, outer scroll을 less scroll
         if innerScroll && lessScroll {
             guard innerScrollView.contentOffset.y < 0 && outerScrollView.contentOffset.y > 0 else { return }
-            let moveOffset = abs(innerScrollView.contentOffset.y) / Metric.reduceAcceacceleration
+            let moveOffset = abs(innerScrollView.contentOffset.y) / Metric.reduceAcceleration
             outerScrollView.contentOffset.y = max(outerScrollView.contentOffset.y - moveOffset, 0)
         }
         
